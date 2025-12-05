f250f2aea412a10209090b89828fa46fadc65867
diegruanoperez2006@gmail.com <diegoruanoperez2006@gmail.com>
2025-12-05 17:43:10 +0100

    Tauri Project Inizialization

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./Global.css";
+
+function App() {
+  return (
+    <main className="bg-blue-200 text-6xl w-creen h-screen">
+      <section className="w-full h-full grid place-items-center ">
+        Proyecto inicializado + tailwind
+      </section>
+    </main>
+  );
+}
+
+export default App;
