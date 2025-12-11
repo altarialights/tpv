dcabcd6475c7c0994f76fd696f1665c5d218c5b3
altarialights <altarialights@gmail.com>
2025-12-11 19:10:46 +0100

    logo and svgs

--- src/components/layoutComponent/NavBar.tsx
+++ src/components/layoutComponent/NavBar.tsx
@@ -1,18 +1,47 @@
 import NavBarItem from "./NavBarItem";
-import { ReactComponent as MesaSVG } from '../../assets/svg/MesaSVG.svg'
+import { ReactComponent as MesaSVG } from "../../assets/svg/MesaSVG.svg";
+import { ReactComponent as MenuOpen } from "../../assets/svg/MenuOpen.svg";
+import { ReactComponent as MenuClose } from "../../assets/svg/MenuClose.svg";
+import Logo from "../../assets/img/logo.png";
 
 function NavBar({ rol }: { rol: string }) {
     if (rol == "admin") {
         return (
             <section>
                 <header>
-                    <img src="" alt="" />
-                    <svg></svg>
+                    <img src={Logo} alt="" />
+                    <MenuOpen width={32} height={32} />
+                    <MenuClose width={32} height={32} />
                     <svg></svg>
                 </header>
                 <nav>
                     <div>
-                        <NavBarItem pagina='Mesas' svg={MesaSVG}></NavBarItem>
+                        <NavBarItem pagina="Mesas" svg={MesaSVG}></NavBarItem>
+                        <NavBarItem
+                            pagina="Inventario"
+                            svg={MesaSVG}
+                        ></NavBarItem>
+                        <NavBarItem
+                            pagina="Reservas"
+                            svg={MesaSVG}
+                        ></NavBarItem>
+                        <NavBarItem
+                            pagina="Trabajadores"
+                            svg={MesaSVG}
+                        ></NavBarItem>
+                        <NavBarItem
+                            pagina="Proveedores"
+                            nombre="Proveedores y Pedidos"
+                            svg={MesaSVG}
+                        ></NavBarItem>
+                        <NavBarItem
+                            pagina="Clientes"
+                            svg={MesaSVG}
+                        ></NavBarItem>
+                        <NavBarItem
+                            pagina="Estadisticas"
+                            svg={MesaSVG}
+                        ></NavBarItem>
                     </div>
                     <div>
                         <div>
--- src/components/layoutComponent/NavBarItem.tsx
+++ src/components/layoutComponent/NavBarItem.tsx
@@ -1,9 +1,11 @@
 import { Link, To } from "react-router-dom";
 function NavBarItem({
     pagina,
+    nombre,
     svg: SvgIcon,
 }: {
     pagina: string;
+    nombre?: string;
     svg: React.FC<React.SVGProps<SVGSVGElement>>;
 }) {
     const ruta: To = "../" + pagina;
@@ -12,7 +14,7 @@ function NavBarItem({
         <Link to={ruta}>
             <div>
                 <SvgIcon width={32} height={32} />
-                <h4>{pagina}</h4>
+                <h4>{nombre || pagina}</h4>
             </div>
         </Link>
     );
